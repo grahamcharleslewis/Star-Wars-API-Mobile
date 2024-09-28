69f70958e0142adc1a1bd2195dff921c4bc0bad4
Graham Charles Lewis <grahamcharleslewis@gmail.com>
2024-09-28 10:20:28 +0100

    Initial commit

---
+++ app/src/main/java/com/example/starwarsapimobile/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.starwarsapimobile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.starwarsapimobile.ui.theme.StarWarsAPIMobileTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            StarWarsAPIMobileTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    https://swapi.dev/api/
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    StarWarsAPIMobileTheme {
+        Greeting("Android")
+    }
+}
